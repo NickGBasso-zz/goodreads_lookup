2b4c36edc4c0205dcd79943d61cd95ba1062e3ea
Nick Basso <nick.basso@shopify.com>
2018-03-28 16:07:17 -0400

    Fixed problem with getBook not being recognized.

--- lib/goodreads_lookup.rb
+++ lib/goodreads_lookup.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'nokogiri'
 
 module GoodreadsLookup
-  def getBook(isbn)
+  def self.getBook(isbn)
     goodReadsBook = Nokogiri.XML(open("https://www.goodreads.com/search/index.xml?key=IJIOijQmGxMt06aqcZOmmQ&q={#{isbn}}"))
     title = /<title>(.+?)<\/title>/.match(goodReadsBook.at_xpath("//title").to_s).captures[0]
     name = /<name>(.+?)<\/name>/.match(goodReadsBook.at_xpath("//name").to_s).captures[0]
